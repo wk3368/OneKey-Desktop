0cbe06a735c29009f726d2aa2d81207ac9fa402e
loatheb <loatheb.zhao@gmail.com>
2021-03-15 16:45:46 +0800

    fix: 默认关闭 passphrase 保护

--- packages/suite/src/constants/suite/device.ts
+++ packages/suite/src/constants/suite/device.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_LABEL = 'My OneKey';
-export const DEFAULT_PASSPHRASE_PROTECTION = true;
+export const DEFAULT_PASSPHRASE_PROTECTION = false;
 export const DEFAULT_SKIP_BACKUP = true;
 export const DEFAULT_STRENGTH_T1 = 256;
 export const DEFAULT_STRENGTH_T2 = 128;
